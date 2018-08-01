f9c25d02f9d2a9a762461446e24572f0c7a4e780
Matt Hensley <matt.hensley777@gmail.com>
2018-07-31 20:46:02 -0500

    add

---
+++ main.js
@@ -0,0 +1,11 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+function addingTwoNumbers () {
+
+	var a = 5;
+	var b = 3;
+	var sum = a + b;
+
+	console.log("sum", sum);
+
+};
