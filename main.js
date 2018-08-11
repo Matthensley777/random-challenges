3804bd2074e53f757528c99a3c0446d88a2021df
Matt Hensley <matt.hensley777@gmail.com>
2018-08-10 20:03:13 -0500

    fix

--- main.js
+++ main.js
@@ -25,10 +25,7 @@ var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 // The stackLetter function should accept the array as the sole argument
 function stackLetters (alphabet) {
 }
-    /*
-      Write a `for` loop that iterates over the array argument and
-      outputs the letters.
-     */
+    
  
 var aStack = [];
  for (var i = 0; i < alphabet.length; i++) {
@@ -42,5 +39,4 @@ var aStack = [];
  }
 
 
-// Invoke the function and pass in the array
 stackLetters(alphabet)
